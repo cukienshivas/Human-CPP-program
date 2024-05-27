f431b95044f7081d4e0c94e244c4c9cc05d8b2a5
cukienshivas <121390191+cukienshivas@users.noreply.github.com>
2024-05-27 13:45:17 +0700

    Add files via upload

---
+++ Con_nguoi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class bills{
+    string bill_ID;
+    string cuatomer_ID;
+    float total_cost;
+            };
+int main(){
+    std::cout<<"Hello world"<<std::endl;
+    std::cout<<"GOODBYE"<<std::endl;
+    std::cout << "THIS IS 31 HOUR OF CODING CPP" << endl;
+
+    return 0;
+}
